b12f665b7fa699b0b5e5062b55e9d208c3b543af
Ricky Tsao <rtsao6680@gmail.com>
2020-05-15 12:21:36 +0800

    removed test stub

--- hello.js
+++
@@ -1,8 +0,0 @@
-
-export function hello(text) {
-    // const div = document.createElement('div');
-    // div.textContent = `Hello ${text}`;
-    // document.body.appendChild(div);
-
-    console.log('hello eberybady');
-  }
