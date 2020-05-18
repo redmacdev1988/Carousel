d2ab40a54fb19947ec4b8f2d768585c4f60ab2e1
Ricky Tsao <Ricky_Tsao@emap.com>
2020-05-18 18:05:45 +0800

    applied green sock animation

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// tween
+console.log('ran tween');
+
+//gsap.from(".logo", {duration: 1.5, opacity: 0, scale: 0.3, ease: "back"});
+
+//gsap.from(".circle", {duration: 1, opacity: 0, y: () => Math.random() * 400 - 200, stagger: 0.25});
+
+
+gsap.to('.logo', { // config obj
+    duration: 1.0,
+    x: 60,
+    borderRadius: '20%',
+    ease:'elastic', // bounce, back
+    onComplete: () => {
+
+    }
+});
+
+
+
+/*
+gsap.set('.logo', {tarnsformOrigin: "50% 50%"});
+gsap.to('.logo', {duration: 2, rotation: 360});
+
+*/
+
+/*
+var myObject = {rotation: 0};
+gsap.to(myObject, {duration: 2, rotation: 360, onUpdate: function() {
+    console.log(myObject.rotation);
+}});
+*/
+
+// onStart
+
+
+
+
